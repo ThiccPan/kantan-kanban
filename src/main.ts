a6384a04de2b4496ca3bb934eb1b7cf7fb97ca12
ThiccPan <ihilmiderian@gmail.com>
2024-03-01 14:57:33 +0700

    add new task feature

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import KanbanCard from "./kanbanCard";
 import KanbanStore from "./store"
 
 let board = document.querySelector("#board-container") as HTMLDivElement
+let taskInput = document.querySelector("#task-input") as HTMLInputElement
+let submitBtnInput = document.querySelector("#task-submit-btn") as HTMLButtonElement
 
 export const kanbanStore = new KanbanStore()
 
@@ -12,5 +14,8 @@ let doingBoard = new KanbanBoard("board-2", "doing");
 backlogBoard.render(board)
 doingBoard.render(board)
 
-let task1 = new KanbanCard("task 1")
-task1.render(backlogBoard)
+submitBtnInput.onclick = (ev) => {
+    console.log(taskInput.value)
+    let task1 = new KanbanCard(taskInput.value, backlogBoard.id)
+    task1.render(backlogBoard)
+}
