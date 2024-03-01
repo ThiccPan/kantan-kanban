959ca6356a6b754a9709c521f30aee57754f7d86
ThiccPan <ihilmiderian@gmail.com>
2024-03-01 14:57:09 +0700

    update task api

--- src/store.ts
+++ src/store.ts
@@ -18,4 +18,34 @@ export default class KanbanStore {
         localStorage.setItem(boardId, JSON.stringify(parsedData))
         return true
     }
+
+    update(oldBoardId: string, data: { task: KanbanCard, newBoardId: string }) {
+        console.log(`old board: ${oldBoardId} -> new board: ${data.newBoardId}`)
+        let fetched = localStorage.getItem(oldBoardId)
+        let parsedData: boardStorage = {
+            tasks: []
+        }
+        if (fetched) {
+            parsedData = JSON.parse(fetched)
+        }
+
+        console.log("before: ", parsedData.tasks)
+        console.log(data.task.id)
+        parsedData.tasks = parsedData.tasks.filter((task) => task.id !== data.task.id)
+        console.log("after: ", parsedData.tasks)
+        localStorage.setItem(oldBoardId, JSON.stringify(parsedData))
+
+        let newFetched = localStorage.getItem(data.newBoardId)
+        let newparsedData: boardStorage = {
+            tasks: []
+        }
+
+        if (newFetched) {
+            newparsedData = JSON.parse(newFetched)
+        }
+
+        newparsedData.tasks.push(data.task)
+        localStorage.setItem(data.newBoardId, JSON.stringify(newparsedData))
+        return true
+    }
 }
