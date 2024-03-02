b284435197d782a0df90c9c51ebc4301265d5405
ThiccPan <ihilmiderian@gmail.com>
2024-03-02 14:35:14 +0700

    load task from store

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,43 @@ import KanbanBoard from "./kanbanBoard";
 import KanbanCard from "./kanbanCard";
 import KanbanStore from "./store"
 
-let board = document.querySelector("#board-container") as HTMLDivElement
+let boardContainer = document.querySelector("#board-container") as HTMLDivElement
 let taskInput = document.querySelector("#task-input") as HTMLInputElement
 let submitBtnInput = document.querySelector("#task-submit-btn") as HTMLButtonElement
+let clearBtn = document.querySelector('#clear-task') as HTMLButtonElement
 
 export const kanbanStore = new KanbanStore()
 
-let backlogBoard = new KanbanBoard("board-1", "backlog");
-let doingBoard = new KanbanBoard("board-2", "doing");
+let boards: Map<string, KanbanBoard> = new Map()
+boards.set("board-1", new KanbanBoard("board-1", "backlog"))
+boards.set("board-2", new KanbanBoard("board-2", "doing"))
 
-backlogBoard.render(board)
-doingBoard.render(board)
+boards.forEach(board => {
+    board.render(boardContainer)
+});
 
 submitBtnInput.onclick = (ev) => {
     console.log(taskInput.value)
-    let task1 = new KanbanCard(taskInput.value, backlogBoard.id)
-    task1.render(backlogBoard)
+    let task1 = new KanbanCard({ task: taskInput.value, boardId: boards.get("board-1")!.id })
+    task1.render(boards.get(task1.currBoardId)!)
+    task1.add(task1.currBoardId)
+}
+
+clearBtn.onclick = (ev) => {
+    localStorage.clear()
+    location.reload()
+}
+
+window.onload = () => {
+    boards.forEach(board => {
+        let backlogTask = kanbanStore.getAll(board.id)
+        console.log(backlogTask)
+        backlogTask.tasks.forEach((card) => {
+            new KanbanCard({
+                id: card.id,
+                task: card.task,
+                boardId: board.id
+            }).render(board.element)
+        })
+    });
 }
